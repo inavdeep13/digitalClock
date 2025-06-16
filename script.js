f3e939f9da9c00d5ad60cfe7e848360382d23486
inavdeep13 <inavdeep13@gmail.com>
2025-06-16 14:07:43 +0530

    Finish digital clock implementation

--- script.js
+++ script.js
@@ -12,9 +12,9 @@ const showTime = () => {
     let mins = date.getMinutes();
     let secs = date.getSeconds();
 
-    hr = hr < 10 ? `${hr}` : hr;
-    mins = mins < 10 ? `${mins}` : mins;
-    secs = secs < 10 ? `${secs}` : secs;
+    hr = hr < 10 ? `0${hr}` : hr;
+    mins = mins < 10 ? `0${mins}` : mins;
+    secs = secs < 10 ? `0${secs}` : secs;
 
     time.innerHTML = `${hr} : ${mins} : ${secs}` ;
 
